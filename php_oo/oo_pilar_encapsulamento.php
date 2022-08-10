e180872f8d728d51f3e974000959394372bb1a92
Maicon Douglas <maicondsoledade@gmail.com>
2022-08-10 15:23:33 -0300

    aulas php

--- php_oo/oo_pilar_encapsulamento.php
+++ php_oo/oo_pilar_encapsulamento.php
@@ -3,11 +3,21 @@ class Pai{
 private $nome = 'maicon';
 protected $sobrenome = 'soledade';
 public $humor = 'nada';
+
+public function getNome(){
+    return $this->nome;
+}
+public function setNome($value){
+    if(strlen($value)>=3){
+        $this->nome = $value;
+
+    }
+
+}
 }
 $pai = new Pai();
-echo $pai->$humor;
-$pai->$humor = 'triste';
+echo $pai->getNome();
+$pai->setNome('douglas');
 echo '<br/>';
-echo $pai->$humor;
-
+echo $pai->getNome();
 ?>
